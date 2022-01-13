3f68b7f1c90d47b342f104c6d72be09c1670f808
RXRAJA <rajanaec.12@gmail.com>
2022-01-13 08:34:42 +0530

    push 2

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,74 @@
 const express = require('express');
-
 const router = express.Router();
+const User = require('../Models/User');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res)=>{
-    res.send('Get loggged in user')
-})
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    return res.json(user);
+  } catch (err) {
+    return res.status(500).send('Server Error');
+  }
+});
 
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res)=>{
-    res.send('Log in user')
-})
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email.').isEmail(),
+    check('password', 'Enter your password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'invalid credentials' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'invalid credentials' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to the contact keeper API'}));
+//Connect DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the contact keeper API' })
+);
 
 // Define routes
 app.use('/api/users', require('./routes/users'));
@@ -11,4 +20,4 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server started on PORT: ${PORT}.`))
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}.`));
